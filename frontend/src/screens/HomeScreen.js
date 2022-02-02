e5e903004d3627c111d122cd1e25b9e0bba3c4a4
Adam Tamminga <tammingaar28@gmail.com>
2022-02-01 18:59:16 -0600

    Setting up redux on homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 function HomeScreen() {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        async function fetchProducts(){
-
-            const {data} = await axios.get('/api/products/')
-            setProducts(data)
-        }
-
-        fetchProducts()
+        dispatch(listProducts())
         
     }, [])
 
+    const products = []
     return (
         <div>
             <h1>Latest Products</h1>
